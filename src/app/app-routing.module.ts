e8f8698af4f8db0a276e7c45d797500e571846af
k4ys4r <hilal_balout@hotmail.com>
2020-01-20 13:45:33 +0100

    correction formule detailes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { FormuleListComponent } from './formule-list/formule-list.component';
 import { LoggingComponent } from './logging/logging.component';
 import { AuthGuardService } from './shared/auth-guard.service';
+import { FormuleDetailsComponent } from './formule-details/formule-details.component';
 
 
 const routes: Routes = [
   { path: 'clientlogin', component: LoggingComponent },
   { path: 'gerervoyage', component: FormuleListComponent,canActivate:[AuthGuardService] },
   { path: 'formulelist', component: FormuleListComponent },
+  {path: 'formule/:id',component: FormuleDetailsComponent},
   { path: '**', redirectTo: 'formulelist' },
 ];
 
