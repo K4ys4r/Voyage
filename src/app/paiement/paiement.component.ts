618851bcb398f4e2a292f5a609248fd967da4bb2
jeremycailloux <jeremycailloux@gmail.com>
2020-01-21 12:35:33 +0100

    finir le paiement

--- src/app/paiement/paiement.component.ts
+++ src/app/paiement/paiement.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Carte } from '../model/Carte';
+import * as moment from 'moment'
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-paiement',
@@ -10,10 +12,12 @@ import { Carte } from '../model/Carte';
 export class PaiementComponent implements OnInit {
 
   public cartePaiement: Carte;
-  constructor() { }
+  constructor(private router: Router) { }
 
 
   clientCarte: FormGroup;
+  public paiementStatus = false;
+
 
   ngOnInit() {
     this.clientCarte = new FormGroup({
@@ -24,15 +28,35 @@ export class PaiementComponent implements OnInit {
     })
   }
 
-  onSubmit(){
+  onSubmit() {
     this.cartePaiement = this.clientCarte.value;
     this.verifierCarte();
+    this.paiementStatus = true;
   }
 
 
-  verifierCarte(){
-    
-    const date = this.cartePaiement.date_expiration
+  verifierCarte() {
 
+    var dateCarte = moment(this.cartePaiement.date_expiration)
+    var aujourdhui = moment();
+    var duration = moment.duration(dateCarte.diff(aujourdhui));
+    if (duration.milliseconds() < 0) {
+      alert('Carte non valide');
+      this.clientCarte.controls.date_expiration.setValue('');
+    };
+
+    if(this.cartePaiement.cryptogramme.toString().length != 3){
+      alert('Cryptogramme incorrect');
+      this.clientCarte.controls.cryptogramme.setValue('');
     }
+
+    if(this.cartePaiement.numero.toString().length != 16){
+      alert('Numero de carte incorrect');
+      this.clientCarte.controls.numero.setValue('');
+    }
+  }
+
+  linkFormule(){
+    this.router.navigate(['/formulelist'])
+  }
 }
