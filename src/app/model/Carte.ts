9cea360f6b2788303d6db5b3cad6ffbde51d585a
jeremycailloux <jeremycailloux@gmail.com>
2020-01-21 11:13:43 +0100

    carte model

---
+++ src/app/model/Carte.ts
@@ -0,0 +1,17 @@
+export class Card {
+    public titulaire: string;
+    public numero: number;
+    public cryptogramme: number;
+    public dateExpiration: Date;
+    public validite: boolean; 
+
+    constructor(titulaire: string, numero: number, cryptogramme: number, dateExpiration: Date, validite: boolean){
+
+        this.titulaire = titulaire;
+        this.numero = numero;
+        this.cryptogramme = cryptogramme;
+        this.dateExpiration = dateExpiration;
+        this.validite = validite;
+    }
+
+}
